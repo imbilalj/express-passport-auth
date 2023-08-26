100864b6eda5db0d62c07deaa4aa94c0222360fd
Bilal Javed <imbilalj@gmail.com>
2023-08-26 13:09:33 +0500

    Add mongoose pre save hook to user schema for password hashing

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema(
   {
@@ -12,5 +13,14 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const saltRounds = 10;
+
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
 
 export const User = mongoose.model('User', UserSchema);
