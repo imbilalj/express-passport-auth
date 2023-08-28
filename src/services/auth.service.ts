e8bd1d35e0fdb4d33ca0d76a08c395f60563d130
Bilal Javed <imbilalj@gmail.com>
2023-08-28 12:36:56 +0500

    Add signAccessToken method to auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,22 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.model';
 
 export const validatePassword = async (
   plainPassword: string,
   hashedPassword: string
 ) => {
   return await bcrypt.compare(plainPassword, hashedPassword);
+};
+
+export const signAccessToken = (user: User) => {
+  const accessToken = jwt.sign(
+    { _id: user._id, email: user.email },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: '1d',
+    }
+  );
+
+  return accessToken;
 };
