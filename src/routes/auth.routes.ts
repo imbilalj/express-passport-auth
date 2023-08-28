59953b5c2ad3eccc612093901d0e292fce30ea6e
Bilal Javed <imbilalj@gmail.com>
2023-08-28 12:39:56 +0500

    Fix login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { validateRequest } from '../middlewares/request-validator.middleware';
+import { LoginSchema } from '../schemas/auth.schema';
+import { loginUserHandler } from '../controllers/auth.controller';
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-  res.send({ message: 'User logged in' });
-});
+router.post('/login', validateRequest(LoginSchema), loginUserHandler);
 
 export default router;
